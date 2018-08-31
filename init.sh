2678534f24e71c127cd8adad30ef551afb4e0e3e
Geraldi Karim <geraldi.karim@gmail.com>
2018-08-31 09:48:45 +0700

    Xmodmap mappings

--- init.sh
+++ init.sh
@@ -2,10 +2,12 @@
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -sf $dir/zshrc ~/.zshrc
-
 ln -sf $dir/gemrc ~/.gemrc
 
 ln -sf $dir/gitconfig ~/.gitconfig
 
 ln -sf $dir/gitignore-global ~/.gitignore-global
+
+ln -sf $dir/xmodmap ~/.xmodmap
+
+ln -sf $dir/zshrc ~/.zshrc
